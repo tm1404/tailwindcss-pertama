e49349a8ec3ef6fa3a26bd8977593c642aa368f2
Muhamad Taufik <tm14041994@gmail.com>
2022-02-04 14:04:29 +0700

    commit pertama ada petir

---
+++ _dist/script.js
@@ -0,0 +1,6 @@
+const toggleResponsif = document.querySelector(".toggle-responsif")
+const menuResponsif = document.querySelector(".menu-responsif")
+
+toggleResponsif.addEventListener("click", () => {
+  menuResponsif.classList.toggle("hidden")
+})
